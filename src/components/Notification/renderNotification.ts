02b3474a48a80c1ef8d1f6f4d4c1cd27bd078453
Yevhenii <33032868+sharovskiye@users.noreply.github.com>
2022-02-05 20:49:56 +0200

    RS-20 feat(notification): add render notification and bootstrap (#23)
    * RS-20 feat(notification): add render notification and bootstrap
    
    * RS-20 fix(Notification): add default values, correct files path and container for notification
    
    * RS-20 fix(Notification): fix position

---
+++ src/components/Notification/renderNotification.ts
@@ -0,0 +1,31 @@
+import { Toast } from 'bootstrap';
+import PaletteBootstrap from '../constants';
+
+const notificationContainer = document.createElement('div');
+notificationContainer.classList.add('toast-container', 'position-fixed', 'top-0', 'end-0', 'p-3');
+document.body.append(notificationContainer);
+
+export default (message = 'Error', palette = PaletteBootstrap.error) => {
+  const layout = document.createElement('div');
+  layout.classList.add('toast', palette.bg, palette.text, 'border-0');
+  layout.setAttribute('role', 'alert');
+  layout.setAttribute('aria-live', 'assertive');
+  layout.setAttribute('aria-atomic', 'true');
+
+  layout.innerHTML = `
+    <div class="d-flex">
+        <div class="toast-body">
+          ${message}
+        </div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" 
+         data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>`;
+
+  layout.addEventListener('hidden.bs.toast', () => {
+    layout.remove();
+  });
+
+  notificationContainer.append(layout);
+  const toast = new Toast(layout);
+  toast.show();
+};
---
+++ src/components/enum.ts
@@ -0,0 +1,29 @@
+export enum BackgroundBootstrap {
+  primary = 'bg-primary',
+  secondary = 'bg-secondary',
+  success = 'bg-success',
+  danger = 'bg-danger',
+  warning = 'bg-warning',
+  info = 'bg-info',
+  light = 'bg-light',
+  dark = 'bg-dark',
+  body = 'bg-body',
+  white = 'bg-white',
+  transparent = 'bg-transparent',
+}
+
+export enum TextColorBootstrap {
+  primary = 'text-primary',
+  secondary = 'text-secondary',
+  success = 'text-success',
+  danger = 'text-danger',
+  warning = 'text-warning',
+  info = 'text-info',
+  light = 'text-light',
+  dark = 'text-dark',
+  body = 'text-body',
+  muted = 'text-muted',
+  white = 'text-white',
+  blackOpacity = 'text-black-50',
+  whiteOpacity = 'text-white-50',
+}
