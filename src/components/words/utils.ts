44b6268587c8a27eda388d633b2993de3a4bbb83
Yevhenii <sharovskiyevgeniy@gmail.com>
2022-02-14 01:09:03 +0200

    RS-18 feat(words):add toggle difficulty

--- src/components/words/utils.ts
+++ src/components/words/utils.ts
@@ -1,7 +1,8 @@
 import { BASE_LINK } from '../../services/constants';
+import fetchWithErrorHandling from '../../services/fetchWithErrorHandling';
 import authStore from '../../store/authStore';
-import { Difficulty } from '../enum';
-import { IBodyWord } from '../interfaces';
+import { Difficulty, Method } from '../enum';
+import { IBodyWord, IResponseBodyWord } from '../interfaces';
 
 export async function getWordInfo(token: string) {
   const url = `${BASE_LINK}users/${authStore.userId}/words/${token}`;
@@ -14,14 +15,19 @@ export async function getWordInfo(token: string) {
   return false;
 }
 
-const GOOD_HARD_SCORE = 5;
-const GOOD_NORMAL_SCORE = 3;
-
-export function updateBody(win: boolean, body?: IBodyWord) {
-  const currentBody = body || {
-    difficulty: Difficulty.normal,
+function createBodyWord(hard = false) {
+  return {
+    difficulty: hard ? Difficulty.hard : Difficulty.normal,
     optional: { streak: 0, wins: 0, losses: 0, learned: false },
   };
+}
+
+export function updateBody(win: boolean, body?: IBodyWord) {
+  const GOOD_HARD_SCORE = 5;
+  const GOOD_NORMAL_SCORE = 3;
+
+  const currentBody = body || createBodyWord();
+
   if (win) {
     currentBody.optional.streak += 1;
     currentBody.optional.wins += 1;
@@ -41,4 +47,30 @@ export function updateBody(win: boolean, body?: IBodyWord) {
     currentBody.difficulty = Difficulty.normal;
   }
   return currentBody;
+}
+
+export async function fetchWord(token: string, method: string, body: IBodyWord) {
+  const url = `${BASE_LINK}users/${authStore.userId}/words/${token}`;
+  const headers = new Headers({ 'Content-Type': 'application/json', Authorization: `Bearer ${authStore.token}` });
+
+  const request = await fetchWithErrorHandling(url, () => {}, {
+    method,
+    body: JSON.stringify(body),
+    headers,
+  });
+  console.log(request);
+}
+
+export async function toggleDifficulty(token = '5e9f5ee35eb9e72bc21af410') {
+  const body = (await getWordInfo(token)) as IResponseBodyWord;
+  const method = body ? Method.PUT : Method.POST;
+  if (body) {
+    body.difficulty = body.difficulty === Difficulty.normal ? Difficulty.hard : Difficulty.normal;
+    const { difficulty, optional } = body;
+    fetchWord(token, method, { difficulty, optional });
+  } else {
+    const isHard = true;
+    const hardBody = createBodyWord(isHard);
+    fetchWord(token, method, hardBody);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import './sass/main.scss';
 
 import renderAllMarkup from './pages/allMarkup';
-import updateWord from './components/words/updateWord';
 
 renderAllMarkup();
-
-// const token = '5e9f5ee35eb9e72bc21af4a2';
-// const body: IBodyWord = { difficulty: 'low', optional: { streak: 1, wins: 0, losses: 0, learned: true } };
-
-updateWord();
