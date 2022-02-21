f32c5c38ec8275aef7cf68ea9ddad4ad895993e3
Yevhenii <33032868+sharovskiye@users.noreply.github.com>
2022-02-21 09:49:24 +0200

    RS-32 feat(token): add refresh token (#46)

--- src/services/fetchWithErrorHandling.ts
+++ src/services/fetchWithErrorHandling.ts
@@ -1,12 +1,48 @@
 import { PaletteBootstrap } from '../components/constants';
 import { IRequests } from '../components/interfaces';
 import renderNotification from '../components/Notification/renderNotification';
+import authStore from '../store/authStore';
+import { BASE_LINK } from './constants';
 import findErrorMessage from './utils';
+
+interface IRetoken {
+  token: string;
+  refreshToken: string;
+}
 
 export default async <T>(request: IRequests): Promise<T | undefined> => {
   const { url, finallyCallback, options = {}, showNotification } = request;
   try {
     const res = await fetch(url, options);
+
+    if (res.status === 401 || res.status === 402) {
+      const headersForRetoken = new Headers({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authStore.refreshToken}`,
+      });
+      const retoken: IRetoken = await (
+        await fetch(`${BASE_LINK}users/${authStore.userId}/tokens`, { headers: headersForRetoken })
+      ).json();
+
+      authStore.token = retoken.token;
+      authStore.refreshToken = retoken.refreshToken;
+      localStorage.setItem('auth', JSON.stringify(authStore));
+
+      const newHeaders = new Headers({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authStore.token}`,
+      });
+      options.headers = newHeaders;
+      const newRes = await fetch(url, options);
+
+      if (!newRes.ok) {
+        const textError = await newRes.text();
+        throw new Error(textError);
+      }
+
+      return await newRes.json();
+    }
+
     if (!res.ok) {
       const textError = await res.text();
       throw new Error(textError);
@@ -14,8 +50,6 @@ export default async <T>(request: IRequests): Promise<T | undefined> => {
     return await res.json();
   } catch (e) {
     const errMessage = (e as Error).message;
-    // console.log(errMessage);
-    // if (errMessage === 'Access token is missing or invalid'){}
     if (showNotification) {
       renderNotification(findErrorMessage(errMessage), PaletteBootstrap.error);
     }
